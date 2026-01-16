f19df5b0fba403002bb8cc0eb03781925677a04f
nidjasim <polamekkartnija@gmail.com>
2026-01-16 11:50:27 +0100

    scroll

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const passportWrap = document.querySelector('.passport-wrap');
 document.addEventListener('scroll', ()=>{
     const height = Math.floor(document.documentElement.scrollTop) / 10;
     passportWrap.style.minHeight = `${height}em`;
+    console.log(height)
+    if(height > 250) {
+
+    }
 })
